672e74428236d062d94f458b41b653cefa73b0a3
Cristhian Fuentes <cristhianf3193@gmail.com>
2024-10-23 12:05:37 -0500

    se actualizó dos archivos index.html y script.js añadiendo titulo y nuevo items

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const liveCountdownElement = document.getElementById("live-countdown");
+    const totalDaysElement = document.getElementById("total-days");
     const monthlyDatesElement = document.getElementById("monthly-dates");
 
     const startDate = new Date("October 9, 2024 00:00:00");
@@ -14,6 +15,10 @@ document.addEventListener("DOMContentLoaded", () => {
         const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
 
         liveCountdownElement.innerHTML = `${days} días, ${hours} horas:, ${minutes} minutos:, ${seconds} Segundos`;
+    
+        // Calculate total days including partial days
+        const totalDays = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
+        totalDaysElement.innerHTML = `Total de días: ${totalDays}`;
     }
 
     setInterval(updateCountdown, 1000);
